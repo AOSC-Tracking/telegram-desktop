6989404582c4d940393dd906ec28097322edd0bd
Mingcong Bai <jeffbai@aosc.io>
2024-04-09 22:02:32 +0800

    fix(ui): fix build with Qt 5
    Co-authored-by: Henry Chen <chenx97@aosc.io>

--- Telegram/lib_ui/ui/text/text_entity.cpp
+++ Telegram/lib_ui/ui/text/text_entity.cpp
@@ -2404,6 +2404,8 @@ EntityInText::EntityInText(
 , _data(data) {
 }
 
+EntityInText::EntityInText() {}
+
 int EntityInText::FirstMonospaceOffset(
 		const EntitiesInText &entities,
 		int textLength) {
